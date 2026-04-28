ea55880838a6cf559c043a0668f9ada5dc1994f5
MiguelCaroan0 <miguel.caroano@escola.pr.gov.br>
2026-04-28 09:58:24 -0300

    String e Number

---
+++ type-string.js
@@ -0,0 +1,9 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Testando";
+const senha = "senha-segura";
+const StringDeNumeros = "12345";
+
+const citacao = "Meu nome é: ";
+const meuNome = "Miguel Caroano";
+
+console.log(citacao + meuNome);
