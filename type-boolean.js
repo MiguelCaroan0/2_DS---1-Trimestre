e7b3a90edaa9dbe379e1c78d8e5a55e1eb33090d
MiguelCaroan0 <miguel.caroano@escola.pr.gov.br>
2026-04-28 10:16:29 -0300

    Adicionando type-boolean (verdadeira ou falso)

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero = 3;
+let cadastroAtivado = true;
+
+const texto1 = "Alura";
+const texto2 = "Alura2"
+
+console.log(texto1 === texto2);
+
+let int = numero;
+
+console.log("Qual seu número de identificação?: ");
