b853360a4ee73bf102a7463ec50e5ac70e0671cc
MiguelCaroan0 <miguel.caroano@escola.pr.gov.br>
2026-05-05 10:29:25 -0300

    criando template e tenarion

---
+++ template.js
@@ -0,0 +1,9 @@
+nomeUsuario = 'Miguel';
+sobrenomeUsuario = 'Caroano';
+idadeUsuario = 2021 - 1980;
+nacionalidade  = 'brasileiro';
+
+const apresentacao = `Meu nome é ${nomeUsuario} ${sobrenomeUsuario}, tenho ${idadeUsuario} anos e sou ${nacionalidade}`; 
+
+console.log(apresentacao);
+
---
+++ tenarion.js
@@ -0,0 +1,20 @@
+// function conferirirIdade( idadeMininma = 18, idadeUsuario){
+//     if (idadeUsuario >= idadeMInima){
+//         return 'Cerveja'
+//     }
+//     else{
+//         return 'Suco'
+//     }
+// }
+
+// console.log(conferirIdade(18, 17));
+
+const idadeMinima = 18;
+let idadeUsuario = 17;
+
+if (idadeUsuario >= idadeMinima){
+    return console.log('Cerveja');
+}
+else{
+    return console.log('Suco');
+}
