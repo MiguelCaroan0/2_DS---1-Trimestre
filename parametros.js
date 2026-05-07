9130b60aa7aa6b443a3844b2275de4f7ec52b29f
MiguelCaroan0 <miguel.caroano@escola.pr.gov.br>
2026-05-07 10:59:34 -0300

    Criando parametros

---
+++ parametros.js
@@ -0,0 +1,13 @@
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e eu tenho ${idade} anos`;
+}
+
+function soma(num1, num2){
+     return num1 + num2;
+}
+
+function multiplicacao(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiplicacao(soma(2,4)));
